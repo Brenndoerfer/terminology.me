73951386753cd05a71096367d61ecf8bdf95b0f0
Michael Brenndoerfer <mbnet@hotmail.de>
2021-08-29 20:20:39 +0800

    term sugg colors safelisted

--- components/modular/TermSuggestions.tsx
+++ components/modular/TermSuggestions.tsx
@@ -2,6 +2,7 @@ import { TERM_PATH } from '../../lib/constants';
 import Link from 'next/link';
 import classNames from 'classnames';
 import { domainShortToColor } from '../../lib/transformer';
+import overrideTailwindClassNames from '../../lib/overrideTailwind';
 
 
 export interface ITermSuggestions {
@@ -26,8 +27,8 @@ export default function TermSuggestions(props: TermSuggestionsProp) {
                 {props.terms && props.terms.map((term) => ((
                     <div className="xl:w-1/4 md:w-1/2 p-4" key={term.title}>
                         <Link href={`${TERM_PATH}/[slug]`} as={`${TERM_PATH}/${term.slug}`} shallow={false}><a>
-                            <div className={classNames("bg-white p-4 rounded-md border shadow-sm border-gray-100 hover-fade", "border-gray-100", 'hover:' + domainShortToColor(term.domainShort).mediumBorder)}>
-                                <h3 className={classNames("tracking-wied text-xs font-medium uppercase", domainShortToColor(term.domainShort).text)}>{term.domain}</h3>
+                            <div className={overrideTailwindClassNames(classNames("bg-white p-4 rounded-md border shadow-sm hover-fade", "border-gray-100", 'hover:' + domainShortToColor(term.domainShort).mediumBorder))}>
+                                <h3 className={overrideTailwindClassNames(classNames("tracking-wied text-xs font-medium uppercase", domainShortToColor(term.domainShort).text))}>{term.domain}</h3>
                                 <h2 className="text-lg text-gray-900 font-medium title-font mt-1">{term.title}</h2>
                             </div>
                         </a></Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,48 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    purge: {
+        content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+        options: {
+            safelist: [
+                'border-red-100',
+                'border-blue-100',
+                'border-green-100',
+                'border-red-100',
+                'border-orange-100',
+                'border-blueGray-100',
+                'border-warmGray-100',
+                'border-red-300',
+                'border-blue-300',
+                'border-green-300',
+                'border-red-300',
+                'border-orange-300',
+                'border-blueGray-300',
+                'border-warmGray-300',
+                'hover:border-red-300',
+                'hover:border-blue-300',
+                'hover:border-green-300',
+                'hover:border-red-300',
+                'hover:border-orange-300',
+                'hover:border-blueGray-300',
+                'hover:border-warmGray-300',
+                'border-red-500',
+                'border-blue-500',
+                'border-green-500',
+                'border-red-500',
+                'border-orange-500',
+                'border-blueGray-500',
+                'border-warmGray-500',
+                'text-red-600',
+                'text-blue-600',
+                'text-green-600',
+                'text-red-600',
+                'text-orange-600',
+                'text-blueGray-600',
+                'text-warmGray-600',
+            ]
+        }
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         colors: {
